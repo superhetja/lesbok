0afa96410d6315edc8db6eba33e7ba11b63a6491
Sindrit17 <sindrit17@ru.is>
2022-02-20 16:45:26 +0000

    jest/test

--- app/src/services/backend.ts
+++ app/src/services/backend.ts
@@ -1,8 +1,9 @@
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'http://localhost:3000';
 
-export const getUsers = async() => {
+export const getUsers = async () => {
     return await fetch(BASE_URL + '/users', {
-        method: 'GET'
-    }).then(response => response.json())
-    .catch(error => console.log('error: ', error))
-}
+        method: 'GET',
+    })
+        .then((response) => response.json())
+        .catch((error) => console.log('error: ', error));
+};
