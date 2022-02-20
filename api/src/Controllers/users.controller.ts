043dc87a9035e5271852b9008448a7b5ea4f9da8
superhetja <42982342+superhetja@users.noreply.github.com>
2022-02-20 14:04:57 +0000

    database import and export scripts

--- api/src/Controllers/users.controller.ts
+++ api/src/Controllers/users.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post } from "@nestjs/common";
 import { UsersService } from "src/Services/users.services";
+import { User } from "src/Models/user.model";
 
 @Controller()
 export class UsersController {
@@ -8,6 +9,11 @@ export class UsersController {
     @Get('/user')
     getUser(): string {
         return this.userService.getUser();
+    }
+
+    @Get('/users')
+    getUsers(): Promise<User []> {
+        return this.userService.findAll();
     }
 
     @Post('/createMany')
---
+++ app/src/services/backend.ts
@@ -0,0 +1,8 @@
+const BASE_URL = 'http://localhost:3000'
+
+export const getUsers = async() => {
+    return await fetch(BASE_URL + '/users', {
+        method: 'GET'
+    }).then(response => response.json())
+    .catch(error => console.log('error: ', error))
+}
