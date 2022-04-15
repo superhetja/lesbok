04e0e8306cd832ed97b723424ad9e0016659f21f
Sindrit17 <sindrit17@ru.is>
2022-04-15 16:31:11 +0000

    dashboard latest entry

--- api/src/entry/entry.service.ts
+++ api/src/entry/entry.service.ts
@@ -64,7 +64,8 @@ export class EntryService {
 				},
 			},
 		});
-		return score/;
+		console.log('score: ', score);
+		return score;
 	}
 
 	// eslint-disable-next-line class-methods-use-this, @typescript-eslint/no-unused-vars
--- app/src/utils/helpers.ts
+++ app/src/utils/helpers.ts
@@ -6,5 +6,5 @@ export function getDateNow() {
 
 export function getDateFormated(unFormated: string) {
 	let date = new Date(unFormated);
-	return `${date.getDate()}. ${date.getMonth()}. ${date.getFullYear()}`
+	return `${date.getDate()}. ${date.getMonth()+1}. ${date.getFullYear()}`
 }
