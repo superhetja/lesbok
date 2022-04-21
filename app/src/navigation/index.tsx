3f7a06e69a8d78411c6956eaf5220d4bbf60251c
Sindrit17 <sindrit17@ru.is>
2022-04-21 23:10:54 +0000

    small fix

--- app/src/navigation/index.tsx
+++ app/src/navigation/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import ProfileScreenNavigationProp,{ NavigationContainer, useNavigation } from '@react-navigation/native';
 import ListScreen from '../screens/ListScreen/listScreen';
 import DashboardScreen from '../screens/DashboardScreen/dashboardScreen';
 import LoginScreen from '../screens/LoginScreen/loginScreen';
@@ -7,21 +7,31 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import BottomNavigation from '../components/Navigations/bottomNavigation';
 import Notification from '../screens/Notification/notification';
 import SetNotifications from '../components/Notifications/Notifications';
+import { Button } from '@ui-kitten/components';
+
 
 
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
+
 const HomeNavigator = () => (
-  <Navigator tabBar={props => <BottomNavigation {...props} />}>
-		<Screen name='Mælaborð' component={DashboardScreen} />
-    <Screen name='Seinustu færslur' component={ListScreen}/>
-		<Screen name='Innskráning' component={SetNotifications} />
+	// const navigation: <ProfileScreenNavigationProp>;
+		// const navigation = useNavigation()
+			<Navigator tabBar={props => <BottomNavigation {...props} />}>
+			<Screen name='Mælaborð' component={DashboardScreen} />
+    <Screen
+			name='Seinustu færslur'
+			component={ListScreen}
+			options={{
+				headerRight: () => <Button onPress={useNavigation()}>Click me</Button>
+			}}/>
+		<Screen name='Stillingar' component={SetNotifications} />
   </Navigator>
 );
 
 export const AppNavigator = () => (
-  <NavigationContainer>
+	<NavigationContainer>
     <HomeNavigator/>
   </NavigationContainer>
 );
