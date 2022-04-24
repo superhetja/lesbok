49d1388b4e847eb1df9cc4827bbeea20892e08f5
BergurThor <bergura@gmail.com>
2022-04-24 11:43:48 +0000

    Small fixes
    Dashes removed from users SSNs and  fixed warnings in the roles seed file.

--- api/seeders/20220418174749-demo-users.js
+++ api/seeders/20220418174749-demo-users.js
@@ -10,7 +10,7 @@ module.exports = {
 			{
 				id: '84f575f0-58a0-47e4-835f-dce5591622df',
 				name: 'Embla Kristjánsdóttir',
-				national_id: '200294-2999',
+				national_id: '2002942999',
 				email: 'embla@stekkjaskoli.is',
 				created: new Date(),
 				modified: new Date(),
@@ -18,7 +18,7 @@ module.exports = {
 			{
 				id: 'b5e44efa-2966-470c-888a-dfd7ceb5be8c',
 				name: 'Arnar Baldursson',
-				national_id: '200294-2989',
+				national_id: '2002942989',
 				email: 'arnar@stekkjaskoli.is	',
 				created: new Date(),
 				modified: new Date(),
@@ -26,7 +26,7 @@ module.exports = {
 			{
 				id: '28f8ee3d-d63f-48b7-8be3-a102231f3bf5',
 				name: 'Jóhann Lárusson',
-				national_id: '200294-2930',
+				national_id: '2002942930',
 				email: 'johann@stekkjaskoli.is',
 				created: new Date(),
 				modified: new Date(),
@@ -34,7 +34,7 @@ module.exports = {
 			{
 				id: '8d823a90-2617-4ffb-b4e3-765a29f98da6',
 				name: 'Ormur Magnússon',
-				national_id: '200294-2599',
+				national_id: '2002942599',
 				email: 'ormur@stekkjaskoli.is',
 				created: new Date(),
 				modified: new Date(),
@@ -42,7 +42,7 @@ module.exports = {
 			{
 				id: 'c19a9fe6-b43f-46bd-9263-f59514122d7c',
 				name: 'Pálína Nökkvadóttir',
-				national_id: '200296-2999',
+				national_id: '2002962999',
 				email: 'palina@vallaskoli.is',
 				created: new Date(),
 				modified: new Date(),
@@ -50,7 +50,7 @@ module.exports = {
 			{
 				id: '9310ccea-f958-49e5-91de-5bc000f53c46',
 				name: 'Ragnar Guðmundsson',
-				national_id: '200294-3099',
+				national_id: '2002943099',
 				email: 'ragnar@vallaskoli.is',
 				created: new Date(),
 				modified: new Date(),
@@ -58,7 +58,7 @@ module.exports = {
 			{
 				id: 'dc7f1d86-8a7c-48bf-a4f4-5fd4e5fb38e3',
 				name: 'Tinna Vignisdóttir',
-				national_id: '200284-2999',
+				national_id: '2002842999',
 				email: 'tinna@vallaskoli.is',
 				created: new Date(),
 				modified: new Date(),
@@ -66,7 +66,7 @@ module.exports = {
 			{
 				id: '541535da-5a38-4fb1-8d6c-90eff0d56b50',
 				name: 'Freyja Arnarsdóttir',
-				national_id: '200294-2559',
+				national_id: '2002942559',
 				email: 'freyja@vallaskoli.is',
 				created: new Date(),
 				modified: new Date(),
@@ -74,7 +74,7 @@ module.exports = {
 			{
 				id: 'ed219c4e-7fd4-4f9b-9f11-2dc0d2b48e73',
 				name: 'Bergur Þór',
-				national_id: '200294-2929',
+				national_id: '2002942929',
 				email: 'bergura@gmail.com',
 				created: new Date(),
 				modified: new Date(),
@@ -82,7 +82,7 @@ module.exports = {
 			{
 				id: '05ad625f-a584-4c18-b1be-1bc390f06114',
 				name: 'Bertha Eygló',
-				national_id: '200194-9429',
+				national_id: '2001949429',
 				email: 'bertha@gmail.com',
 				created: new Date(),
 				modified: new Date(),
@@ -90,7 +90,7 @@ module.exports = {
 			{
 				id: '9902e245-254b-4c30-86b6-7c80f2230572',
 				name: 'Sindri Snær Þorsteinsson',
-				national_id: '280294-2929',
+				national_id: '2802942929',
 				email: 'sindrismai@gmail.com',
 				created: new Date(),
 				modified: new Date(),
--- api/seeders/20220422113815-demo-roles.js
+++ api/seeders/20220422113815-demo-roles.js
@@ -1,24 +1,22 @@
-'use strict';
-
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+	async up(queryInterface, Sequelize) {
+		/**
+		 * Add seed commands here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkInsert('People', [{
+		 *   name: 'John Doe',
+		 *   isBetaMember: false
+		 * }], {});
+		 */
+	},
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+	async down(queryInterface, Sequelize) {
+		/**
+		 * Add commands to revert seed here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkDelete('People', null, {});
+		 */
+	},
 };
