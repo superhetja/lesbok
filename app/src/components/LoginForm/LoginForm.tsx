b0083315d0a58d48e9ed55928d8a6cce15966c9e
Sindrit17 <sindrit17@ru.is>
2022-04-24 13:08:14 +0000

    added more seeders

--- app/src/components/LoginForm/LoginForm.tsx
+++ app/src/components/LoginForm/LoginForm.tsx
@@ -5,7 +5,7 @@ import {showMessage} from 'react-native-flash-message'
 import { TextInput } from "../FormComponents";
 import styles from "./styles";
 
-const DEFAULT_VALUE = '200294-2930';
+const DEFAULT_VALUE = '2002942930';
 
 type FormData = {
 	telephone: string;
